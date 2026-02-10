762f98ef8382ee7bded7da3a9302542e4e02f366
Martin Matzer <martin.matzer2@avl.com>
2026-02-10 23:08:35 +0100

    Add new methods and implementing game board logic
    - new method add_coin()
    - implementation of add_coin()
    - implementation of reset_board()
    - implementation of check_for_draw

--- src/connect-4/game_board.py
+++ src/connect-4/game_board.py
@@ -41,18 +41,62 @@ class GameBoard:
         print("\n   +----+----+----+----+----+----+----+")
         self._print_board_footer()
 
+    def add_coin(self, color: str, column: int) -> bool:
+        """
+        Method for adding a new coin into the game board.
+        This method takes two parameter and returns True if the coin can be added to the given column of the board.
+        It will return False f the coin cannot be added anymore to the given column because it is full.
+        :param color: str - "red" or "yellow" - color of the coin which should be added to the board
+        :param column: int - number of the column where the coin should be added
+        :return: bool - True for success and False if coin cannot be added anymore
+        """
+
+        # Check if the given column is already full
+        if self._board[0][column - 1] != "":
+            return False
+        else:
+            # Add coin to the lowest possible row
+            for i in range(self._rows - 1, -1, -1):
+                if self._board[i][column - 1] == "":
+                    if color == "red":
+                        self._board[i][column - 1] = "🔴"
+                        return True
+                    else:
+                        self._board[i][column - 1] = "🟡"
+                        return True
+
+    def check_for_winner(self) -> str:
+        pass
+
+    def check_for_draw(self) -> bool:
+        """
+
+        :return:
+        """
+
+        draw = True
+
+        for i in range(0, self._columns):
+            if self._board[0][i] == "":
+                draw = False
+
+        return draw
+
 
     def reset_board(self):
-        pass
+        """
+        Method for removing all coins from the game board for starting a new game.
+        :return:
+        """
 
-    def check_turn(self, column: int):
-        pass
-
-    def check_for_winner(self):
-        pass
-
-    def check_for_draw(self):
-        pass
+        self._board = [
+            ["", "", "", "", "", "", ""],
+            ["", "", "", "", "", "", ""],
+            ["", "", "", "", "", "", ""],
+            ["", "", "", "", "", "", ""],
+            ["", "", "", "", "", "", ""],
+            ["", "", "", "", "", "", ""],
+        ]
 
 
 if __name__ == "__main__":
