d2dce389dbcb1b23f849f1ef11acd44930248023
Martin Matzer <martin.matzer2@avl.com>
2026-02-11 22:00:32 +0100

    Implement check_for_winner() and docstring improvements

--- src/connect-4/game_board.py
+++ src/connect-4/game_board.py
@@ -3,8 +3,8 @@
 class GameBoard:
 
     def __init__(self):
-        self._rows = 6
-        self._columns = 7
+        self._ROW_COUNT = 6
+        self._COLUMN_COUNT = 7
         self._possibleColumns = ["0", "1", "2", "3", "4", "5", "6"]
         self._board = [
             ["", "", "", "", "", "", ""],
@@ -25,16 +25,19 @@ class GameBoard:
     def print_board(self):
         """
         Method for printing the current state of the game board to the command line.
+
+        Returns
+        -------
         :return:
         """
         self._print_board_header()
         print("\n     0    1    2    3    4    5    6   ", end="")
 
-        for x in range(self._rows):
+        for x in range(self._ROW_COUNT):
             print("\n   +----+----+----+----+----+----+----+")
             print(x, " |", end="")
 
-            for y in range(self._columns):
+            for y in range(self._COLUMN_COUNT):
                 if self._board[x][y] == "🟡":
                     print("", self._board[x][y], end=" |")
                 elif self._board[x][y] == "🔴":
@@ -47,12 +50,23 @@ class GameBoard:
 
     def add_coin(self, color: str, column: int) -> bool:
         """
-        Method for adding a new coin into the game board.
-        This method takes two parameter and returns True if the coin can be added to the given column of the board.
+        Method for adding a new coin into the game board if the turn is valid.
+
+        This method takes two parameters (color and column) and returns True if the coin can be added to the given column of the board.
         It will return False f the coin cannot be added anymore to the given column because it is full.
-        :param color: str - "red" or "yellow" - color of the coin which should be added to the board
-        :param column: int - number of the column where the coin should be added
-        :return: bool - True for success and False if coin cannot be added anymore
+
+        Parameters
+        ----------
+        :param color: str - "red" or "yellow"
+            Color of the coin which should be added to the board
+        :param column: int
+            Number of the column where the coin should be added
+
+        Returns
+        -------
+        :return: bool
+            True for a valid turn and successful adding to game board.
+            False for a not valid turn when coin cannot be added to the given column.
         """
 
         # Check if the given column is already full
@@ -60,7 +74,7 @@ class GameBoard:
             return False
         else:
             # Add coin to the lowest possible row
-            for i in range(self._rows - 1, -1, -1):
+            for i in range(self._ROW_COUNT - 1, -1, -1):
                 if self._board[i][column] == "":
                     if color == "red":
                         self._board[i][column] = "🔴"
@@ -70,29 +84,79 @@ class GameBoard:
                         return True
 
     def _count_direction(self, row: int, column: int, delta_row: int, delta_column: int, color: str):
-
-        if color == "red":
-            coin = "🔴"
-        else:
-            coin = "🟡"
-
-        count = 0
+        """
 
 
+        Parameters
+        ----------
+        :param row:
+        :param column:
+        :param delta_row:
+        :param delta_column:
+        :param color:
 
+        Returns
+        -------
+        :return:
+        """
 
-    def check_for_winner(self) -> str:
         pass
+
+    def check_for_winner(self, coin) -> bool:
+        """
+        Check the board for winner with the provided coin.
+
+        Method will return True if the board already has 4 piece of the provided coin in row, column or diagonal.
+
+        Parameters
+        ----------
+        :param coin: str
+            Type of coin for which the win check is executed.
+
+        Returns
+        -------
+        :return: bool
+            True if the provided coin has won.
+            False if the provided coin has not won yet.
+        """
+
+        # check for horizontal win
+        for r in range(self._ROW_COUNT):
+            for c in range(self._COLUMN_COUNT - 3):
+                if self._board[r][c] == coin and self._board[r][c + 1] == coin and self._board[r][c + 2] == coin and self._board[r][c + 3] == coin:
+                    return True
+
+
+        # check for vertical win
+        for r in range(self._ROW_COUNT - 3):
+            for c in range(self._COLUMN_COUNT):
+                if self._board[r][c] == coin and self._board[r + 1][c] == coin and self._board[r + 2][c] == coin and self._board[r + 3][c] == coin:
+                    return True
+
+        # check for positive diagonal win
+        for r in range(self._ROW_COUNT - 3):
+            for c in range(self._COLUMN_COUNT - 3):
+                if self._board[r][c] == coin and self._board[r + 1][c + 1] == coin and self._board[r + 2][c + 2] == coin and self._board[r + 3][c + 3] == coin:
+                    return True
+
+        # check for negative diagonal win:
+        for r in range(3, self._ROW_COUNT):
+            for c in range(self._COLUMN_COUNT - 3):
+                if self._board[r][c] == coin and self._board[r - 1][c + 1] == coin and self._board[r - 2][c - 2] == coin and self._board[r - 3][c - 3] == coin:
+                    return True
 
     def check_for_draw(self) -> bool:
         """
         Method to check if the game board is already full. This would mean that the game ended with draw.
+
+        Returns
+        -------
         :return: bool - True if draw otherwise False
         """
 
         draw = True
 
-        for i in range(0, self._columns):
+        for i in range(0, self._COLUMN_COUNT):
             if self._board[0][i] == "":
                 draw = False
 
@@ -102,6 +166,9 @@ class GameBoard:
     def reset_board(self):
         """
         Method for removing all coins from the game board for starting a new game.
+
+        Returns
+        -------
         :return:
         """
 
