34958678f42a4fc8e0ce3776a2ea97b04d2dc534
Melina Fellner <msophie.fellner@gmail.com>
2026-02-12 22:44:27 +0100

    Add functionality to game setup
    Functionality for method pick_starting_player and method create_players (renamed from start_computer_opponent)
    Issue #4

--- src/connect-4/GameSetup.py
+++ src/connect-4/GameSetup.py
@@ -1,3 +1,6 @@
+import random
+from ClassesPlayers import HumanPlayer, Bot
+
 # Class for game setup
 # Selection for number of players and starting player
 class GameSetup:
@@ -14,9 +17,28 @@ class GameSetup:
             print("Invalid input. Please enter 1 or 2.\n")
     
     # Initializes bot
-    def start_computer_opponent(self):
-        pass
+    def create_players(self, num_players):
+        players = []
+        for i in range(num_players):
+            player = HumanPlayer(f"p{i+1}")
+            players.append(player)
+        if num_players == 1:
+            players.append(Bot("p2"))
+        return players
 
     # Selects starting player
-    def pick_starting_player(self):
-        pass
+    def pick_starting_player(self, players:list):
+        starter = random.choice(players)
+        print(f"{starter.color} beginnt!")
+        starter.onturn = True
+
+#Tests for created methods
+#Will be removed after final testing
+setup = GameSetup()
+num_players = setup.choose_players()
+print(num_players)
+players = setup.create_players(num_players)
+print(players)
+print(players[0], players[1])
+
+setup.pick_starting_player(players)
