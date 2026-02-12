5c4ab581847382c83300a6600d4a3d988de6aed5
Melina Fellner <msophie.fellner@gmail.com>
2026-02-12 23:40:14 +0100

    Change to protected attributes for class players
    Change for important attributes to status protected and added getter methods

--- src/connect-4/ClassesPlayers.py
+++ src/connect-4/ClassesPlayers.py
@@ -7,26 +7,35 @@ class Players(ABC):
     _next_player_no = 1
 
     # Player No cannot be assigned manually - done by system
-    def __init__(self):
-        self.__player_no = Players._next_player_no
+    def __init__(self, name):
+        self._player_no = Players._next_player_no
         Players._next_player_no += 1
+        self._name = _name
         self.onturn = False
         self.total_wins = 0
         self.total_turns = 0
         self.current_turns = 0
         #How should playing colors be assigned? Bug with more than 2 objects expected
-        if self.__player_no == 1:
-            self.color = "red"
+        if self._player_no == 1:
+            self._color = "red"
         else:
-            self.color = "yellow"
+            self._color = "yellow"
 
     def __str__(self):
         return f"Player{self.player_no} : {self.color}"
 
+    #Getter for protected attributes
     @property
     def player_no(self):
-        return self.__player_no
-
+        return self._player_no
+    
+    @property
+    def name(self):
+        return self._name
+    
+    @property
+    def color(self):
+        return self._color
 
     @abstractmethod
     def play_turn(self):
