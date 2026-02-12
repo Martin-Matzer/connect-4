452fab61634b6276163e8edd4c9da99e866fdd5a
Melina Fellner <msophie.fellner@gmail.com>
2026-02-12 15:01:26 +0100

    Add logic for method choose_players
    Basic logic for method choose_players was added.

--- src/connect-4/GameSetup.py
+++ src/connect-4/GameSetup.py
@@ -4,7 +4,14 @@ class GameSetup:
 
     #Defines number of human players and if bot is needed
     def choose_players(self) -> int:
-        pass
+        while True:
+            print("Choose number of players:")
+            print("1 - single player")
+            print("2 - two player")
+            selection = input("> ").strip()
+            if selection in ("1", "2"):
+                return int(selection)
+            print("Invalid input. Please enter 1 or 2.\n")
     
     # Initializes bot
     def start_computer_opponent(self):
@@ -13,6 +20,3 @@ class GameSetup:
     # Selects starting player
     def pick_starting_player(self):
         pass
-
-
-
