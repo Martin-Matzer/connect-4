bcad420b04c9283162c5d1a8ad4cea703b8e2824
Melina Fellner <msophie.fellner@gmail.com>
2026-02-12 12:50:32 +0100

    Add new file GameSetup.py
    Add new file with class GameSetup and methods from issue #4. No functionality yet.

---
+++ src/connect-4/GameSetup.py
@@ -0,0 +1,18 @@
+# Class for game setup
+# Selection for number of players and starting player
+class GameSetup:
+
+    #Defines number of human players and if bot is needed
+    def choose_players(self) -> int:
+        pass
+    
+    # Initializes bot
+    def start_computer_opponent(self):
+        pass
+
+    # Selects starting player
+    def pick_starting_player(self):
+        pass
+
+
+
