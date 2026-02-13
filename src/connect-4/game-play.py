693403f620e273e9fdb5fc83fbbff30e2c031233
Martin Matzer <martin.matzer2@avl.com>
2026-02-13 07:43:28 +0100

    Implement first basic game loop

--- src/connect-4/game-play.py
+++ src/connect-4/game-play.py
@@ -13,10 +13,16 @@ setup = GameSetup()
 num_players = setup.choose_players()
 #Create player objects
 players = setup.create_players(num_players)
+player1 = players[0]
+player2 = players[1]
+
 #Select starting player
 setup.pick_starting_player(players)
 #Create empty board
 board = GameBoard()
+
+#Print empty board
+board.print_board()
 
 #Play game until one winner or draw
 #Loop needed with methods
@@ -24,6 +30,47 @@ board = GameBoard()
 #add_coin (board)
 #print_board 
 #check_for_winner and check_for_draw
+
+game_over = False
+
+while not game_over:
+
+    while not board.add_coin(player1.color, player1.play_turn()):
+        print("The last turn was not valid. Please enter a valid column.")
+
+    board.print_board()
+
+    if board.check_for_winner(player1.color):
+        print("Player1 has won the game!")
+        game_over = True
+        break
+
+    elif board.check_for_draw():
+        print("Game has ended with a draw.")
+        game_over = True
+        break
+
+    while not board.add_coin(player2.color, player2.play_turn()):
+        print("The last turn was not valid. Please enter a valid column.")
+
+        if board.check_for_winner(player1.color):
+            print("Player1 has won the game!")
+            game_over = True
+            break
+
+        elif board.check_for_draw():
+            print("Game has ended with a draw.")
+            game_over = True
+            break
+
+    board.print_board()
+
+
+
+
+
+
+
 
 #If winner is existing: announce winner
 #In case of draw: announce draw
