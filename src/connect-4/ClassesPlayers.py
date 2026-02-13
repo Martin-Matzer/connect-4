264f315ed14600dbec8fe89fe397b6e399c35860
Melina Fellner <msophie.fellner@gmail.com>
2026-02-13 15:34:59 +0100

    Add class SmartNot
    Add class SmartBot as intelligent opponent. Method play_turn uses two created methods created as support.

--- src/connect-4/ClassesPlayers.py
+++ src/connect-4/ClassesPlayers.py
@@ -39,6 +39,8 @@ class Players(ABC):
         pass
 
 
+
+
 class HumanPlayer(Players):
 
     def play_turn(self):
@@ -51,12 +53,69 @@ class HumanPlayer(Players):
         print(f"Player {self.player_no} surrendered!")
 
 
+
+
 class Bot(Players):
 
     def play_turn(self):
         column = random.randint(0, 6)
         print(f"Hansi plays column {column}")
         return column
+
+
+
+class SmartBot(Players):
+
+    def play_turn(self, board):
+        valid_cols = self._valid_columns(board)
+
+        # 1) Check for win 
+        for column in valid_cols:
+            if self._would_win(board, self.color, column):
+                print(f"{self.name} plays column {column}")
+                return column
+
+        # 2) Check for blocking player
+        opp_color = "yellow" if self.color == "red" else "red"
+        for column in valid_cols:
+            if self._would_win(board, opp_color, column):
+                print(f"{self.name} plays column {column}")
+                return column
+
+        # 3) Prioritize playing middle columns
+        preferred = [3, 2, 4, 1, 5, 0, 6]
+        for column in preferred:
+            if column in valid_cols:
+                print(f"{self.name} plays column {c}")
+                return column
+
+        # Fallback 
+        column = random.choice(valid_cols)
+        print(f"{self.name} plays column {c}")
+        return column
+
+
+    # Supporting method for play_turn()
+    def _valid_columns(self, board):
+        # checks game board for open columns
+        return [c for c in range(7) if board._board[0][c] == ""]
+
+
+    # Supporting method for play_turn()
+    def _would_win(self, board, color, column):
+        target_row = None
+        for row in range(5, -1, -1):
+            if board._board[row][column] == "":
+                target_row = row
+                break
+        if target_row is None:
+            return False
+
+        coin = "🔴" if color == "red" else "🟡"
+        board._board[target_row][column] = coin
+        won = board.check_for_winner(color)
+        board._board[target_row][column] = ""  # undo turn
+        return won
 
 
 
